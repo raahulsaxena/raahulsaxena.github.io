701fbc28bd9d37f63caf9703235eb03a07baaecb
raahulsaxena <165158910+raahulsaxena@users.noreply.github.com>
2025-04-04 06:17:57 +0000

    Deploying to gh-pages from @ raahulsaxena/raahulsaxena.github.io@729afe87dd02b3b1c76934d6b90ae3d591744661 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -51,18 +51,7 @@ ninja.data = [{
               handler: () => {
                 window.location.href = "/blog/";
               },
-            },{id: "post-",
-      
-        title: "",
-      
-      description: "",
-      section: "Posts",
-      handler: () => {
-        
-          window.location.href = "/blog/2025/2025-04-04-choice-of-language/";
-        
-      },
-    },{id: "post-need-a-tree-of-chars-for-efficient-prefix-search-try-tries",
+            },{id: "post-need-a-tree-of-chars-for-efficient-prefix-search-try-tries",
       
         title: "Need a tree of chars for efficient prefix search? Try Tries!",
       
